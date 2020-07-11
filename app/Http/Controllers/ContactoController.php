0348d907983336f38488da59cd66940f87778873
Jose Alberto Jimenez Palacios <beto_jpt@hotmail.com>
2020-07-11 00:44:04 -0500

    correo completo

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -22,7 +22,7 @@ class ContactoController extends Controller
         $subject = "Asunto del correo";
         $for = "beto_jpt@hotmail.com";
         Mail::send('urologos.email',$request->all(), function($msj) use($subject,$for){
-            $msj->from("iscalbertojimenez22@gmail.com","NombreQueApareceráComoEmisor");
+            $msj->from("iscalbertojimenez22@gmail.com","Contacto Sitio Web");
             $msj->subject($subject);
             $msj->to($for);
         });
