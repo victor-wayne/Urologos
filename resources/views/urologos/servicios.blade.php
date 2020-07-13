d861dd09028015155d63e910d3394e1cd1f68cf7
Jose Alberto Jimenez Palacios <beto_jpt@hotmail.com>
2020-07-13 11:07:07 -0500

    interfaces

--- resources/views/urologos/servicios.blade.php
+++ resources/views/urologos/servicios.blade.php
@@ -1,1 +1,101 @@
-woooow
+@include('urologos.partials.nav')
+
+@yield('servicios')
+
+<hr>
+<section class="contact section-seperator" id="contact">
+           woowow
+            
+        </section>
+
+
+
+<!--========== FOOTER ==========-->
+<footer class="footer">
+    <!-- Links -->
+    <div class="section-seperator">
+        <div class="content-md container">
+            <div class="row">
+                <div class="col-sm-2 sm-margin-b-30">
+                    <!-- List -->
+                    <ul class="list-unstyled footer-list">
+                        <li class="footer-list-item"><a href="#body">Home</a></li>
+                        <li class="footer-list-item"><a href="#about">Team</a></li>
+                        <li class="footer-list-item"><a href="#work">Credentials</a></li>
+                        <li class="footer-list-item"><a href="#contact">Contact</a></li>
+                    </ul>
+                    <!-- End List -->
+                </div>
+                <div class="col-sm-2 sm-margin-b-30">
+                    <!-- List -->
+                    <ul class="list-unstyled footer-list">
+                        <li class="footer-list-item"><a href="#">Twitter</a></li>
+                        <li class="footer-list-item"><a href="#">Facebook</a></li>
+                        <li class="footer-list-item"><a href="#">Instagram</a></li>
+                        <li class="footer-list-item"><a href="#">YouTube</a></li>
+                    </ul>
+                    <!-- End List -->
+                </div>
+                <div class="col-sm-3">
+                    <!-- List -->
+                    <ul class="list-unstyled footer-list">
+                        <li class="footer-list-item"><a href="#">Subscribe to Our Newsletter</a></li>
+                        <li class="footer-list-item"><a href="#">Privacy Policy</a></li>
+                        <li class="footer-list-item"><a href="#">Terms &amp; Conditions</a></li>
+                    </ul>
+                    <!-- End List -->
+                </div>
+            </div>
+            <!--// end row -->
+        </div>
+    </div>
+    <!-- End Links -->
+
+    <!-- Copyright -->
+    <div class="content container">
+        <div class="row">
+            <div class="col-xs-6">
+                <img class="footer-logo" src="{{ asset('img/logo-dark.png') }}" alt="flameonepage Logo">
+            </div>
+            <div class="col-xs-6 text-right">
+                <p class="margin-b-0"><a class="fweight-700" href="#">FlameOnePage</a> Theme Powered by: <a class="fweight-700" href="http://ft-seo.ch/">FairTech Studio</a></p>
+            </div>
+        </div>
+        <!--// end row -->
+    </div>
+    <!-- End Copyright -->
+</footer>
+<!--========== END FOOTER ==========-->
+
+<!-- Back To Top -->
+<a href="javascript:void(0);" class="js-back-to-top back-to-top">Top</a>
+
+<!-- JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- CORE PLUGINS -->
+<script src="{{ asset('vendor/jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/jquery-migrate.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+
+<!-- PAGE LEVEL PLUGINS -->
+<script src="{{ asset('vendor/jquery.easing.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/jquery.back-to-top.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/jquery.smooth-scroll.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/jquery.wow.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/swiper/js/swiper.jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/masonry/jquery.masonry.pkgd.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/masonry/imagesloaded.pkgd.min.js') }}" type="text/javascript"></script>
+
+<!-- PAGE LEVEL SCRIPTS -->
+<script src="{{ asset('js/layout.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/components/wow.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/components/swiper.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/components/masonry.min.js') }}" type="text/javascript"></script>
+
+<script src="{{ asset('js/validate/jquery.validate.js') }}" type="text/javascript"></script>
+
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+</body>
+<!-- END BODY -->
+</html>
